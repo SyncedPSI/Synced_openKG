35f02977cdb0c777a6f9b0b72df5d06975563b09
Peipei Wong <wangpp1995@gmail.com>
2019-01-11 11:28:09 +0800

    Add close action to input

--- src/scripts/shared/input.js
+++ src/scripts/shared/input.js
@@ -1,6 +1,22 @@
 export default () => {
   const input = document.getElementById('js-input');
   const search = document.getElementById('js-go-search');
+  const close = document.getElementById('js-close-search');
+
+  input.addEventListener('input', function() {
+    let className = 'search--icon';
+    if (this.value === '') {
+      className += ' t-hidden';
+    }
+
+    close.setAttribute('class', className);
+  });
+
+  close.addEventListener('click', () => {
+    input.value = '';
+
+    close.setAttribute('class', 'search--icon t-hidden');
+  });
 
   search.addEventListener('click', function () {
     const value = input.value;
